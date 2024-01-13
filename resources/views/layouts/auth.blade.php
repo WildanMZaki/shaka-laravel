df5bec50d2a2ba743603d4db71481f59b643b2d3
WildanMZaki <wildanmzaki7@gmail.com>
2024-01-13 16:23:44 +0700

    Ubah logo

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -20,7 +20,7 @@
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('assets') }}/img/favicon/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets') }}/img/favicon/favicon.png" />
 
     @includeIf('layouts.commons.styles')
 
